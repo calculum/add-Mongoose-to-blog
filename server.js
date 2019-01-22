75fc8c22b8d0e609dc88e72c3535cd2d20d76064
bo <clouculum@gmail.com>
2019-01-22 00:05:42 -0700

    update

---
+++ server.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+// Mongoose internally uses a promise-like object,
+// but it's better to make Mongoose use built in es6 promises
+mongoose.Promise = global.Promise;
+
+// config.js is where we control constants for entire
+// app like PORT and DATABASE_URL
+const {PORT, DATABASE_URL} = require('./config');
+const {Restaurant} = require('./models');
